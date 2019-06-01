5bee108d6e098ce63f9a61444fdcfdab30be0cda
Артем Рябцев <riabchik2013@gmail.com>
2019-06-01 07:45:44 +0300

    Initial Commit

---
+++ Splash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splash
+//
+//  Created by Артем Рябцев on 6/1/19.
+//  Copyright © 2019 Артем Рябцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
